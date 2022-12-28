84964dc435bfe50df028b1c5acac5db74fc2d3a5
Tom Spencer <thomas.spencer@codurance.com>
2022-12-28 23:53:20 +0000

    Second version of MergeSort

--- src/main/java/com/tomspencerlondon/mergesort/MergeSort2.java
+++ src/main/java/com/tomspencerlondon/mergesort/MergeSort2.java
@@ -2,15 +2,17 @@ package com.tomspencerlondon.mergesort;
 
 import java.util.Arrays;
 
-public class MergeSort {
+public class MergeSort2 {
 
   public static void main(String[] args) {
     int[] arr = {9, 5, 2, 4, 3};
+
     sort(arr, new int[arr.length], 0, arr.length - 1);
+
     System.out.println(Arrays.toString(arr));
   }
 
-  public static void sort(int[] arr, int[] temp, int low, int high) {
+  private static void sort(int[] arr, int[] temp, int low, int high) {
     if (low < high) {
       int mid = low + (high - low) / 2;
       sort(arr, temp, low, mid);
@@ -20,23 +22,21 @@ public class MergeSort {
   }
 
   private static void merge(int[] arr, int[] temp, int low, int mid, int high) {
-    for (int i = low; i <= high; i++) {
+    for (int i = 0; i < arr.length; i++) {
       temp[i] = arr[i];
     }
-
     int i = low;
     int j = mid + 1;
     int k = low;
 
     while (i <= mid && j <= high) {
-      if (temp[i] < temp[j]) {
+      if (temp[i] <= temp[j]) {
         arr[k] = temp[i];
         i++;
       } else {
         arr[k] = temp[j];
         j++;
       }
-
       k++;
     }
 
