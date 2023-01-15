023d90e6597aa9248e0306a0b4e261463c49d5e8
Tom Spencer <tomspencerlondon@gmail.com>
2023-01-15 15:25:15 +0000

    Remove duplicates from LinkedList with HashSet

---
+++ src/main/java/com/tomspencerlondon/linkedlist/challenge7/Challenge7HashSet.java
@@ -0,0 +1,39 @@
+package com.tomspencerlondon.linkedlist.challenge7;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Challenge7HashSet {
+    public static void main(String[] args) {
+        // Remove duplicates from a linked list
+//      Given a linked list remove duplicates from it
+
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        list.insertAtHead(2);
+        list.insertAtHead(10);
+        list.insertAtHead(7);
+        list.insertAtHead(10);
+
+
+        list.printList();
+        removeDuplicatesWithHashSet(list);
+        list.printList();
+    }
+
+    private static void removeDuplicatesWithHashSet(SinglyLinkedList<Integer> list) {
+        Set<Integer> set = new HashSet<>();
+        Node current = list.headNode;
+        Node prev = null;
+
+        while (current != null) {
+            if (set.contains(current.data)) {
+                prev.nextNode = current.nextNode;
+            } else {
+                set.add((Integer) current.data);
+                prev = current;
+            }
+
+            current = current.nextNode;
+        }
+    }
+}
