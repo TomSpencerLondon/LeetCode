44f2449f6cb245b83d76a7aa61e4c331da2d3ec2
Tom Spencer <tomspencerlondon@gmail.com>
2023-01-15 13:23:56 +0000

    Correct number variable for pushing to Stack and use scoped count from popped count from stack

--- src/main/java/com/tomspencerlondon/stringpractice/DecodeString.java
+++ src/main/java/com/tomspencerlondon/stringpractice/DecodeString.java
@@ -22,35 +22,35 @@ public class DecodeString {
 //       12[ab]
 
 //3[a2[c]]
-//2[abc]3[cd]ef
-//12[ab]
+//
 
-        System.out.println(decode("12[ab]"));
-        System.out.println(decode("3[a2[c]]"));
+//        System.out.println(decode("12[ab]"));
+//        System.out.println(decode("3[a2[c]]"));
+        System.out.println(decode("2[abc]3[cd]ef"));
     }
 
     public static String decode(String input) {
         Stack<String> wordStack = new Stack<>();
         Stack<Integer> countStack = new Stack<>();
-        int count = 0;
+        int number = 0;
         StringBuilder word = new StringBuilder();
 
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             if (Character.isDigit(c)) {
-                count = 10 * count + (c - '0');
+                number = 10 * number + (c - '0');
             } else if (Character.isLetter(c)) {
                 word.append(c);
             } else if (c == '['){
-                countStack.push(count);
+                countStack.push(number);
                 wordStack.push(word.toString());
                 word = new StringBuilder();
-                count = 0;
+                number = 0;
             } else {
                 StringBuilder duplicatedWord = new StringBuilder(wordStack.pop());
-                count = countStack.pop();
+                int count = countStack.pop();
 
-                for (int j = 0; j < count; j++) {
+                for (int j = 1; j < count; j++) {
                     duplicatedWord.append(word);
                 }
                 word = duplicatedWord;
