23448ac0ef62718be0001b0cd1f3354a064e040f
Tom Spencer <tomspencerlondon@gmail.com>
2023-01-16 14:20:07 +0000

    Add reverse function for Array class

--- src/main/java/com/tomspencerlondon/codewithmosh/arrays/Array.java
+++ src/main/java/com/tomspencerlondon/codewithmosh/arrays/Array.java
@@ -1,6 +1,8 @@
 package com.tomspencerlondon.codewithmosh.arrays;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Array {
     private int[] items;
@@ -43,10 +45,10 @@ public class Array {
     }
 
     public void reverse() {
-        int[] newItems = new int[count];
+        int[] newItems = new int[items.length];
 
-        for (int i = 0; i < count; i++) {
-            newItems[i] = items[count - i - 1];
+        for (int i = 0; i < items.length; i++) {
+            newItems[i] = items[items.length - 1 - i];
         }
 
         items = newItems;
@@ -62,6 +64,26 @@ public class Array {
         }
 
         return max;
+    }
+
+    public int[] intersection(Array array) {
+        int[] temp = new int[array.count + items.length];
+        int index = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < array.count; j++) {
+                if (items[i] == array.items[j] && !set.contains(items[i])) {
+                    temp[index++] = items[i];
+                    set.add(items[i]);
+                }
+            }
+        }
+        int[] result = new int[index];
+        for (int i = 0; i < index; i++) {
+            result[i] = temp[i];
+        }
+
+        return result;
     }
 
     public void removeAt(int index) {
--- src/main/java/com/tomspencerlondon/codewithmosh/arrays/Exercise1.java
+++ src/main/java/com/tomspencerlondon/codewithmosh/arrays/Exercise1.java
@@ -1,5 +1,7 @@
 package com.tomspencerlondon.codewithmosh.arrays;
 
+import java.util.Arrays;
+
 public class Exercise1 {
     public static void main(String[] args) {
         Array numbers = new Array(3);
@@ -17,5 +19,28 @@ public class Exercise1 {
 
 //        numbers.removeAt(3);
         System.out.println(numbers.indexOf(50));
+
+        System.out.println(numbers.max());
+
+        Array arrayA = new Array(3);
+        arrayA.insert(10);
+        arrayA.insert(20);
+        arrayA.insert(30);
+        arrayA.insert(40);
+
+        Array arrayB = new Array(3);
+        arrayB.insert(10);
+        arrayB.insert(20);
+        arrayB.insert(30);
+        System.out.println(Arrays.toString(arrayA.intersection(arrayB)));
+
+        Array arrayC = new Array(3);
+        arrayC.insert(10);
+        arrayC.insert(20);
+        arrayC.insert(30);
+        arrayC.print();
+        arrayC.reverse();
+        System.out.println("Reversed numbers");
+        arrayC.print();
     }
 }
