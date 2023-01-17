b06ccdfd344a219aeabe371c40d99f9a4d937021
Tom Spencer <tomspencerlondon@gmail.com>
2023-01-17 10:46:47 +0000

    Add ReverseString to index

--- src/main/java/com/tomspencerlondon/codewithmosh/stacks/ReverseString.java
+++ src/main/java/com/tomspencerlondon/codewithmosh/stacks/ReverseString.java
@@ -6,10 +6,13 @@ public class ReverseString {
     public static void main(String[] args) {
         String result = reverseString("abcd");
         System.out.println(result);
+
+        String reversed = reverseToIndex("abcdefghijk", 3);
+
+        System.out.println(reversed);
     }
 
     private static String reverseString(String input) {
-
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < input.length(); i++) {
@@ -21,6 +24,22 @@ public class ReverseString {
         while (!stack.isEmpty()) {
             result.append(stack.pop());
         }
+
+        return result.toString();
+    }
+
+    public static String reverseToIndex(String input, int index) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i <= index; i++) {
+            stack.push(input.charAt(i));
+        }
+
+        while(!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+
+        result.append(input.substring(index + 1));
 
         return result.toString();
     }
