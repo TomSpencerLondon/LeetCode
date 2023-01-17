015afe033237eed160c2839e35110b2b3cdd5178
Tom Spencer <tomspencerlondon@gmail.com>
2023-01-17 11:31:16 +0000

    Correct isBalanced to use isEmpty() to check if all opening brackets have been closed

--- src/main/java/com/tomspencerlondon/codewithmosh/stacks/BalancedExpressions.java
+++ src/main/java/com/tomspencerlondon/codewithmosh/stacks/BalancedExpressions.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class BalancedExpressions {
     public static void main(String[] args) {
-        String str = "(([1] + <2>))[a]";
+        String str = "(((([1] - 1 + <2>)))[a]";
 
         System.out.println(isBalanced(str));
 
@@ -13,36 +13,45 @@ public class BalancedExpressions {
     public static boolean isBalanced(String input) {
         Stack<Character> stack = new Stack<>();
         for (char ch : input.toCharArray()) {
-            if (ch == '(' || ch == '[' || ch == '{' || ch == '<') {
+            if (ch == '(' || ch == '{' || ch == '<' || ch == '[') {
                 stack.push(ch);
-            } else if (ch == ')') {
+            }
+
+            if (ch == ')') {
+                if (isEmpty(stack)) return false;
+
                 if (stack.peek() == '(') {
                     stack.pop();
-                } else {
-                    return false;
-                }
-            } else if (ch == ']') {
-                if (stack.peek() == '[') {
-                    stack.pop();
-                } else {
-                    return false;
                 }
             } else if (ch == '}') {
+                if (isEmpty(stack)) return false;
+
                 if (stack.peek() == '{') {
                     stack.pop();
-                } else {
-                    return false;
                 }
             } else if (ch == '>') {
+                if (isEmpty(stack)) return false;
+
                 if (stack.peek() == '<') {
                     stack.pop();
-                } else {
-                    return false;
+                }
+            } else if (ch == ']') {
+                if (isEmpty(stack)) return false;
+
+                if (stack.peek() == '[') {
+                    stack.pop();
                 }
             }
 
         }
 
-        return true;
+        return stack.isEmpty();
+    }
+
+    private static boolean isEmpty(Stack<Character> stack) {
+        if (stack.isEmpty()) {
+            return true;
+        }
+        return false;
     }
 }
