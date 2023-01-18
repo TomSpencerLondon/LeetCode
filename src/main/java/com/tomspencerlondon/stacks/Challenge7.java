6194568657e08305d130059e5805ec697d8052c8
Tom Spencer <tomspencerlondon@gmail.com>
2023-01-18 16:32:38 +0000

    Add next greater element function

--- src/main/java/com/tomspencerlondon/stacks/Challenge7.java
+++ src/main/java/com/tomspencerlondon/stacks/Challenge7.java
@@ -1,6 +1,7 @@
 package com.tomspencerlondon.stacks;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Challenge7 {
 
@@ -8,7 +9,30 @@ public class Challenge7 {
         // next greater element using stack
         //
 
-        System.out.println(Arrays.toString(nextGreaterElementNested(new int[]{1, 3, 8, 4, 10, 5})));
+        System.out.println(Arrays.toString(nextGreaterElement(new int[]{1, 3, 8, 4, 10, 5})));
+    }
+
+    private static int[] nextGreaterElement(int[] arr) {
+        int[] result = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (!stack.isEmpty()) {
+                while (!stack.isEmpty() && stack.peek() <= arr[i]) {
+                    stack.pop();
+                }
+            }
+
+            if (stack.isEmpty()) {
+                result[i] = -1;
+            } else {
+                result[i] = stack.peek();
+            }
+
+            stack.push(arr[i]);
+        }
+
+        return result;
     }
 
     public static int[] nextGreaterElementNested(int[] arr) {
