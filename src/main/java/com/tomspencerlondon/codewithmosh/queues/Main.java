dafa4265e0534f90b416a04ddcd648796114c52b
Tom Spencer <tomspencerlondon@gmail.com>
2023-01-19 16:48:11 +0000

    Add queue practice

---
+++ src/main/java/com/tomspencerlondon/codewithmosh/queues/Main.java
@@ -0,0 +1,16 @@
+package com.tomspencerlondon.codewithmosh.queues;
+
+import com.tomspencerlondon.stacks.Queue;
+
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<>(3);
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.dequeue();
+        System.out.println(queue);
+    }
+}
