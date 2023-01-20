01eaf03cd455330cc9ee1259c970aec42fa5f616
Tom Spencer <tomspencerlondon@gmail.com>
2023-01-20 12:00:21 +0000

    ReverseQueue

---
+++ src/main/java/com/tomspencerlondon/codewithmosh/queues/ReverseQueue.java
@@ -0,0 +1,35 @@
+package com.tomspencerlondon.codewithmosh.queues;
+
+import com.tomspencerlondon.stacks.Queue;
+
+import java.util.ArrayDeque;
+import java.util.Stack;
+
+public class ReverseQueue {
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<>(3);
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        System.out.println(queue);
+        reverse(queue);
+        System.out.println(queue);
+    }
+
+    public static void reverse(Queue<Integer> queue) {
+        // add
+        // remove
+        // isEmpty
+
+        Stack<Integer> stack = new Stack<>();
+
+        while (!queue.isEmpty()) {
+            stack.push(queue.dequeue());
+        }
+
+        while (!stack.isEmpty()) {
+            queue.enqueue(stack.pop());
+        }
+    }
+}
