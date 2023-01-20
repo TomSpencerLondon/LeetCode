f3e47517ebbb6f84c698c52652e69ee4c9906aa4
Tom Spencer <tomspencerlondon@gmail.com>
2023-01-20 13:28:09 +0000

    Add PriorityQueue - reorders elements in queue

---
+++ src/main/java/com/tomspencerlondon/codewithmosh/queues/PriorityQueue.java
@@ -0,0 +1,17 @@
+package com.tomspencerlondon.codewithmosh.queues;
+
+public class PriorityQueue {
+
+    public static void main(String[] args) {
+        java.util.PriorityQueue<Integer> queue = new java.util.PriorityQueue<>();
+
+        queue.add(5);
+        queue.add(1);
+        queue.add(3);
+        queue.add(2);
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.remove());
+        }
+    }
+}
