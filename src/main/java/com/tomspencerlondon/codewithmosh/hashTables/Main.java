aa6e10a2d7c4fd517f0410596d89287277c08e30
Tom Spencer <tomspencerlondon@gmail.com>
2023-01-22 17:26:22 +0000

    Add Java map practice

---
+++ src/main/java/com/tomspencerlondon/codewithmosh/hashTables/Main.java
@@ -0,0 +1,35 @@
+package com.tomspencerlondon.codewithmosh.hashTables;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Key: Employee number (Integer)
+        // Value: Name (string)
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Mosh");
+        map.put(2, "John");
+        map.put(3, "Mary");
+        map.put(3, "Marianne");
+        map.put(4, null);
+        map.put(null, null);
+        map.remove(null);
+        String value = map.get(3);
+        System.out.println(value);
+        System.out.println(map.containsKey(3)); // O(1)
+        System.out.println(map.containsValue("Mosh")); // O(n)
+
+        for (Integer key : map.keySet()) {
+            System.out.println(key);
+        }
+
+        for (Map.Entry<Integer, String> keyValuePair : map.entrySet()) {
+            System.out.println(keyValuePair);
+        }
+
+        System.out.println(map);
+    }
+}
