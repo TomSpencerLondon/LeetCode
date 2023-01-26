1081de50f247238467506966a88172053313e8c7
tom <tomspencerlondon@gmail.com>
2023-01-26 09:35:00 +0000

    Add explanatory notes

--- src/main/java/com/tomspencerlondon/codewithmosh/part2nonlinear/binarytrees/mosh/recursion/Main.java
+++ src/main/java/com/tomspencerlondon/codewithmosh/part2nonlinear/binarytrees/mosh/recursion/Main.java
@@ -5,16 +5,21 @@ public class Main {
     public static void main(String[] args) {
         // 4! = 4 x 3 x 2 x 1
 //         3! = 3 x 2 x 1
-
+        // n! = n x (n - 1)!
         System.out.println(factorial(4));
     }
 
+    // f(3)
+    // 3 x f(2)
+    //      2 x f(1)
+    //          1 x f(0)
+    // 3 x 2
+    //      2 x 1
+    //         1 x 1
     public static int factorial(int n) {
-        int factorial = 1;
-        for (int i = n; i > 1; i--) {
-            factorial *= i;
+        if (n <= 0) {
+            return 1;
         }
-
-        return factorial;
+        return n * factorial(n - 1);
     }
 }
