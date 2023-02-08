9fc3b5a0f1ae18dd65c3d4a550fef23bf1f4987e
tom <tomspencerlondon@gmail.com>
2023-02-08 15:41:02 +0000

    Add recursive option for findKthMax

--- src/main/java/com/tomspencerlondon/trees/educative/challenge2/Main.java
+++ src/main/java/com/tomspencerlondon/trees/educative/challenge2/Main.java
@@ -12,7 +12,38 @@ public class Main {
         binarySearchTree.add(2);
         binarySearchTree.add(8);
 
-        System.out.println(findKthMax(binarySearchTree.getRoot(), 3));
+        System.out.println(findKthMaxRec(binarySearchTree.getRoot(), 3));
+    }
+
+    static int counter;
+    public static int findKthMaxRec(Node root, int k) {
+        counter = 0;
+        Node node = findKthMaxRecursive(root, k);
+
+        if (node != null) {
+            return node.getData();
+        }
+
+        return -1;
+    }
+
+    private static Node findKthMaxRecursive(Node root, int k) {
+        if (root == null) {
+            return null;
+        }
+
+        Node node = findKthMaxRecursive(root.getRightChild(), k);
+
+        if (counter != k) {
+            counter++;
+            node = root;
+        }
+
+        if (counter == k) {
+            return node;
+        } else {
+            return findKthMaxRecursive(root.getLeftChild(), k);
+        }
     }
 
     public static int findKthMax(Node root, int k) {
