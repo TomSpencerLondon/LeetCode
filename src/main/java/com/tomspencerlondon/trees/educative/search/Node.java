5552d42d35ccbf800f66134d0f00539afd049408
tom <tomspencerlondon@gmail.com>
2023-02-08 12:04:12 +0000

    Add search function for BinarySearchTree

---
+++ src/main/java/com/tomspencerlondon/trees/educative/search/Node.java
@@ -0,0 +1,38 @@
+package com.tomspencerlondon.trees.educative.search;
+
+public class Node {
+
+    private int data;
+    Node leftChild;
+    Node rightChild;
+
+    Node(int value) {
+        this.data = value;
+        leftChild = null;
+        rightChild = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(Node leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public Node getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(Node rightChild) {
+        this.rightChild = rightChild;
+    }
+}
