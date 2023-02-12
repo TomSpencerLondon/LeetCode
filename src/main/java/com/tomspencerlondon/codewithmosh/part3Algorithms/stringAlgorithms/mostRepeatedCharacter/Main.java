2966d96e2412b9954135d18a575ad313b3498e0a
tom <tomspencerlondon@gmail.com>
2023-02-12 17:47:17 +0000

    Add maxOccrringCharacter using array

--- src/main/java/com/tomspencerlondon/codewithmosh/part3Algorithms/stringAlgorithms/mostRepeatedCharacter/Main.java
+++ src/main/java/com/tomspencerlondon/codewithmosh/part3Algorithms/stringAlgorithms/mostRepeatedCharacter/Main.java
@@ -7,7 +7,28 @@ public class Main {
     public static void main(String[] args) {
         String input = "Hello World";
 
-        System.out.println(getMaxOccuringChar(input));
+        System.out.println(getMaxOccurringCharacter(input));
+    }
+
+    public static char getMaxOccurringCharacter(String str) {
+        final int ASCII_SIZE = 256;
+
+        int[] frequencies = new int[ASCII_SIZE];
+
+        for (char ch : str.toCharArray()) {
+            frequencies[ch]++;
+        }
+
+        int max = 0;
+        char result = ' ';
+        for (int i = 0; i < frequencies.length; i++) {
+            if (frequencies[i] > max) {
+                max = frequencies[i];
+                result = (char) i;
+            }
+        }
+
+        return result;
     }
 
     public static char getMaxOccuringChar(String str) {
