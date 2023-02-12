f8ce6ad1c32d52f886e88937739d166a0842d661
tom <tomspencerlondon@gmail.com>
2023-02-12 19:22:49 +0000

    Add bubble sort algorithm

---
+++ src/main/java/com/tomspencerlondon/algorithms/educative/searching/bubbleSort/Main.java
@@ -0,0 +1,25 @@
+package com.tomspencerlondon.algorithms.educative.searching.bubbleSort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {3, 2, 1, 4, 6, 7, 5};
+
+        bubbleSort(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
