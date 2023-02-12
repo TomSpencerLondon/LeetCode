4f6978d053153dd16e9dd41fd2866008660e017e
tom <tomspencerlondon@gmail.com>
2023-02-12 18:50:24 +0000

    isPalindrome better performant

--- src/main/java/com/tomspencerlondon/codewithmosh/part3Algorithms/stringAlgorithms/palindrome/Main.java
+++ src/main/java/com/tomspencerlondon/codewithmosh/part3Algorithms/stringAlgorithms/palindrome/Main.java
@@ -4,7 +4,20 @@ public class Main {
     public static void main(String[] args) {
         String input = "abccba";
 
-        System.out.println(isPalindrome(input));
+        System.out.println(isPalindrome2(input));
+    }
+
+    public static boolean isPalindrome2(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if (word.charAt(left++) != word.charAt(right--)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static boolean isPalindrome(String word) {
