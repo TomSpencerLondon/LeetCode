8c9d94268b342d5104f57892e397fa7844f883c6
tom <tomspencerlondon@gmail.com>
2023-02-12 19:28:27 +0000

    Add insertion sort

---
+++ src/main/java/com/tomspencerlondon/algorithms/educative/searching/insertionSort/Main.java
@@ -0,0 +1,27 @@
+package com.tomspencerlondon.algorithms.educative.searching.insertionSort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {4, 2, 3, 6, 7, 5};
+
+        insertionSort(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void insertionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int current = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > current) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = current;
+        }
+    }
+}
