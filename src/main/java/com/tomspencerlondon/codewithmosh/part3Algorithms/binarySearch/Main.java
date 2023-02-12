7fac02112d2d63177c618461bd8e5fe8c8cac439
tom <tomspencerlondon@gmail.com>
2023-02-12 16:11:44 +0000

    Add iterative binarySearch

--- src/main/java/com/tomspencerlondon/codewithmosh/part3Algorithms/binarySearch/Main.java
+++ src/main/java/com/tomspencerlondon/codewithmosh/part3Algorithms/binarySearch/Main.java
@@ -4,7 +4,28 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {3, 5, 6, 9, 11, 18, 20, 21, 24, 30};
 
-        System.out.println(binarySearchRec(array, 9));
+        System.out.println(binarySearch(array, 9));
+    }
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (array[middle] == target) {
+                return middle;
+            }
+
+            if (target < array[middle]) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return -1;
     }
 
     private static int binarySearchRec(int[] array, int target) {
