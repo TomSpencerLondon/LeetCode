4c9eac07ec5c046c287dfbc1f6ba63fc6f7c29a2
tom <tomspencerlondon@gmail.com>
2023-02-15 13:20:21 +0000

    Add quickSort

---
+++ src/main/java/com/tomspencerlondon/algorithms/educative/searching/quickSort/Main.java
@@ -0,0 +1,41 @@
+package com.tomspencerlondon.algorithms.educative.searching.quickSort;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {5, 2, 0, 1, 6, 3};
+
+        quickSort(array, 0, array.length - 1);
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int p = partition(array, low, high);
+            quickSort(array, low, p - 1);
+            quickSort(array, p + 1, high);
+        }
+    }
+
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low;
+        int j = low;
+
+        while (i <= high) {
+            if (array[i] <= pivot) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                j++;
+            }
+
+            i++;
+        }
+
+        return j - 1;
+    }
+}
