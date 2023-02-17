a496328d5ae8677e1b4b3a0624ca3997e14fe210
tom <tomspencerlondon@gmail.com>
2023-02-17 19:31:32 +0000

    Add position finder

--- src/main/java/com/tomspencerlondon/harshit/Challenge3.java
+++ src/main/java/com/tomspencerlondon/harshit/Challenge3.java
@@ -16,14 +16,28 @@ public class Challenge3 {
                 {27, 29, 37, 48},
                 {32, 33, 39, 50}
         };
-        System.out.println(Arrays.toString(position(array)));
+        System.out.println(Arrays.toString(position(array, 35)));
     }
 
-    private static int[] position(int[][] array) {
+    private static int[] position(int[][] array, int target) {
+        int column = 0;
+        int row = array.length - 1;
+        int[] result = new int[2];
 
+        while (column < array[0].length && row >= 0) {
+            if (array[column][row] == target) {
+                result[0] = column;
+                result[1] = row;
 
+                return result;
+            }
 
-
+            if (array[column][row] > target) {
+                row--;
+            } else {
+                column++;
+            }
+        }
 
         return new int[]{-1, -1};
     }
