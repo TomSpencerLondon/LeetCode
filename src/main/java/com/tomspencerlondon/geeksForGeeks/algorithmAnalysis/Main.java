04f7da8f4f78378a91e7a191c723a675ff9315b4
tom <tomspencerlondon@gmail.com>
2023-02-18 11:52:21 +0000

    Add third sum function

--- src/main/java/com/tomspencerlondon/geeksForGeeks/algorithmAnalysis/Main.java
+++ src/main/java/com/tomspencerlondon/geeksForGeeks/algorithmAnalysis/Main.java
@@ -3,11 +3,23 @@ package com.tomspencerlondon.geeksForGeeks.algorithmAnalysis;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(sumN2(5));
+        System.out.println(sumN3(5));
     }
 
     private static int sumN2(int n) {
         return n * (n + 1) / 2;
+    }
+
+    private static int sumN3(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                sum++;
+            }
+        }
+
+        return sum;
     }
 
     private static int sumN(int n) {
