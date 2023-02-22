804cea45a46ec46d7b9157c5c2b198a9c8290bf0
tom <tomspencerlondon@gmail.com>
2023-02-22 21:21:23 +0000

    Add search in sorted matrix

---
+++ src/main/java/com/tomspencerlondon/algorithms/educative/searchingAndSorting/challenge6/Main.java
@@ -0,0 +1,36 @@
+package com.tomspencerlondon.algorithms.educative.searchingAndSorting.challenge6;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] matrix  = {{10, 11, 12, 13},
+                {14, 15, 16, 17},
+                {27, 29, 30, 31},
+                {32, 33, 39, 80}};
+
+        int target = 80;
+
+        System.out.println(findKey(matrix, target));
+    }
+
+    private static boolean findKey(int[][] matrix, int target) {
+        int column = 0;
+        int row = matrix.length - 1;
+
+        while (column < matrix[0].length && row >= 0) {
+            if (matrix[row][column] == target) {
+                return true;
+            }
+
+            if (matrix[row][column] > target) {
+                row--;
+            } else {
+                column++;
+            }
+        }
+
+        return false;
+    }
+
+
+}
