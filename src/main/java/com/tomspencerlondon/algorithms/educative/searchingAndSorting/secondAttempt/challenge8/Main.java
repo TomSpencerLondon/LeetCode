33a06a8d33deac0fa7b7ad4631ea63607fa382f6
tom <tomspencerlondon@gmail.com>
2023-02-27 12:42:33 +0000

    Add insertPosition

--- src/main/java/com/tomspencerlondon/algorithms/educative/searchingAndSorting/secondAttempt/challenge8/Main.java
+++ src/main/java/com/tomspencerlondon/algorithms/educative/searchingAndSorting/secondAttempt/challenge8/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         int arr[] = {1,3,5,6};
-        int target = 0;
+        int target = 9;
 
         System.out.println(insertPosition(arr, target));
     }
@@ -18,15 +18,12 @@ public class Main {
             int mid = (left + right) / 2;
 
             if (arr[mid] == target) {
-                if (result == Integer.MIN_VALUE) {
-                    result = mid;
-                }
-                return result;
-            } else if (arr[mid] < target) {
-                result = mid + 1;
+                return mid;
+            } else if (arr[mid] > target) {
+                result = mid;
                 right = mid - 1;
             } else {
-                result = mid;
+                result = mid + 1;
                 left = mid + 1;
             }
         }
