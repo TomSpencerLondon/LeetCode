c832fb00c8a6f128dc7ddb2bbaecbfc469f252c1
tom <tomspencerlondon@gmail.com>
2023-03-12 09:02:00 +0000

    Add LongestCommonSubstring

---
+++ src/main/java/com/tomspencerlondon/algorithms/educative/dynamic/longestCommonSubstring/LongestCommonSubstring.java
@@ -0,0 +1,36 @@
+package com.tomspencerlondon.algorithms.educative.dynamic.longestCommonSubstring;
+
+public class LongestCommonSubstring {
+    public static void main(String[] args) {
+        String first = "www.educative.io/explore";
+        String second = "educative.io/edpresso";
+
+        System.out.println(longestCommonSubstring(first, second));
+    }
+
+    private static int longestCommonSubstring(String first, String second) {
+        int[][] dp = new int[first.length() + 1][second.length() + 1];
+        for (int i = 0; i < second.length(); i++) {
+            if (second.charAt(i) == first.charAt(0)) {
+                dp[0][i] = 1;
+            }
+        }
+
+        int answer = -1;
+        for (int i = 1; i <= first.length(); i++) {
+            for (int j = 1; j <= second.length(); j++) {
+                if (first.charAt(i - 1) == second.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 0;
+                }
+
+                answer = Math.max(answer, dp[i][j]);
+            }
+        }
+
+        return answer;
+    }
+
+
+}
