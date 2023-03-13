53e762f808e5ed7195833667f5547efc2c8292a0
tspencer <tomspencerlondon@gmail.com>
2023-03-13 14:34:53 +0000

    Correct LongestCommonSubsequence

--- src/main/java/com/tomspencerlondon/algorithms/educative/dynamic/strings/LongestCommonSubsequence.java
+++ src/main/java/com/tomspencerlondon/algorithms/educative/dynamic/strings/LongestCommonSubsequence.java
@@ -19,10 +19,6 @@ public class LongestCommonSubsequence {
             dp[0][i] = 0;
         }
 
-        for (int i = 0; i < right.length(); i++) {
-            dp[i][0] = 0;
-        }
-
         for (int i = 1; i <= left.length(); i++) {
             for (int j = 1; j <= right.length(); j++) {
                 if (left.charAt(i - 1) == right.charAt(j - 1)) {
